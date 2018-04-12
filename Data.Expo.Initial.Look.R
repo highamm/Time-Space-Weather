868140a7cb4a27a8a0617dbcbfc00dbeba0cd95d
Matt Higham <highamm@oregonstate.edu>
2018-04-12 10:15:56 -0700

    Subsetted Data

--- Data.Expo.Initial.Look.R
+++ Data.Expo.Initial.Look.R
@@ -1,7 +1,7 @@
 library(readr)
-histWeather <- read_csv("~/Desktop/Data Expo JSM/DataExpo2018/histWeather.csv")
+histWeather <- read_csv("~/Desktop/TimeSpaceExpo/histWeather.csv")
 
-locations <- read_csv("~/Desktop/Data Expo JSM/DataExpo2018/locations.csv")
+locations <- read_csv("~/Desktop/TimeSpaceExpo/locations.csv")
 
 forecast.df <- read.delim("forecast.dat", sep = "") 
 str(data)
@@ -10,3 +10,15 @@ str(histWeather)
 View(forecast.df)
 View(locations)
 View(histWeather)
+
+## merge histWeather and locations
+merged.df <- merge(locations, histWeather, by = "AirPtCd")
+
+## subsetting data to Eugene only for a single time series
+Eug.only <- subset(merged.df, AirPtCd == "KEUG")
+
+## subsetting to a single date so that we have a single spatial process at one
+## particular time
+
+July1.only <- subset(merged.df, Date == "2014-07-01")
+str(July1.only)
