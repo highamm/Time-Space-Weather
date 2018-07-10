48641ae22bcfc7a26d279c6cc77eb236dea7ccaf
Erin Howard <howarder@oregonstate.edu>
2018-07-10 14:41:05 -0700

    Distance plots

--- DistancePlots.R
+++ DistancePlots.R
@@ -5,6 +5,10 @@ allSeasons_F1_replace <- allSeasons_F1[,-c(10,11)]
 
 allSeason_withDists <- merge(allSeasons_F1_replace, dist_locations, by.x = "AirPtCd", 
                              by.y = "AirPtCd")
+
+# puts the facet grid in an order that makes sense 
+allSeason_withDists$season <- factor(allSeason_withDists$season, levels=c("winter", "spring", 
+                                                                          "summer", "fall"))
   
 library(ggplot2)
 
@@ -18,5 +22,14 @@ ggplot(allSeason_withDists, aes(x=distance*0.000621371, y=mean_error, color=fact
   ggtitle("Distance between City Center and Airport vs. \nMean Forecast Error")
 
 
+# If we remove outlier points, still don't see a pattern
+ggplot(allSeason_withDists[allSeason_withDists$distance*0.000621371 < 25, ], aes(x=distance*0.000621371, y=mean_error, color=factor(measure))) + 
+  geom_point() + 
+  facet_grid(.~season) +
+  facet_wrap(~season, ncol=2) + 
+  xlab("Distance (mi)") + 
+  ylab("Mean Forecast Error") + 
+  scale_color_discrete(name="Temperature\nMeasure") + 
+  ggtitle("Distance between City Center and Airport vs. \nMean Forecast Error")
 
 
