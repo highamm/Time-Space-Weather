a9355db24bbafb3d70c95b9755ee0e1baa844a62
Erin Howard <howarder@oregonstate.edu>
2018-07-24 10:06:29 -0700

    prediction model data updated

--- PredictionModelScript.R
+++ PredictionModelScript.R
@@ -1,9 +1,9 @@
 ## self-contained file for the predictive function
 
 
-all.df_completeSub <- read.csv("all_df_completesub.csv")
-nrow(all.df_completeSub)
-colnames(all.df_completeSub)[8] <- "forecastValue"
+#all.df_completeSub <- read.csv("all_df_completesub.csv")
+#nrow(all.df_completeSub)
+#colnames(all.df_completeSub)[8] <- "forecastValue"
 histWeather <- read.csv("histWeather.csv")
 
 library(lubridate)
@@ -11,89 +11,102 @@ library(dplyr)
 library(ggplot2)
 library(lme4)
 
-complete_df <- all.df_completeSub
+#complete_df <- all.df_completeSub
+
+### Not using distance in our model so I will call only the data we need from read.data.R
 
 # include latitude distance in the data set so that it can be included in the model 
-complete_df_dists <- merge(x = complete_df, y = dist_locations[ ,c("AirPtCd", "latDist")], 
+#complete_df_dists <- merge(x = complete_df, y = dist_locations[ ,c("AirPtCd", "latDist")], 
                            by.x = "AirPtCd", by.y = "AirPtCd", all.x=TRUE)
 
-complete_df <- complete_df_dists
-complete_df$Date <- as.Date(complete_df$Date)
+#complete_df <- complete_df_dists
+#complete_df$Date <- as.Date(complete_df$Date)
 
-maxtemp <- subset(complete_df, weathermeas == "MaxTemp")
+#maxtemp <- subset(complete_df, weathermeas == "MaxTemp")
+maxtemp <- maxtempalldat
 nrow(maxtemp)
 
 maxtemp$Date <- as.Date(maxtemp$Date)
 maxtemp$DateofForecast <- as.Date(maxtemp$DateofForecast)
 
 
-maxtemp$month <- month(as.POSIXlt(maxtemp$Date))
+#maxtemp$month <- month(as.POSIXlt(maxtemp$Date))
 
-maxtemp$season <- cut(maxtemp$month, 
-  breaks = c(0.5, 2.5, 5.5, 8.5, 11.5, 12.5), 
-  labels = c("Winter", "Spring", "Summer", "Fall", "Winter2"), 
-  right = FALSE)
+#maxtemp$season <- cut(maxtemp$month, 
+ # breaks = c(0.5, 2.5, 5.5, 8.5, 11.5, 12.5), 
+ # labels = c("Winter", "Spring", "Summer", "Fall", "Winter2"), 
+ # right = FALSE)
 
-maxtemp$season[maxtemp$season == "Winter2"] <- "Winter"
-maxtemp$season <- factor(maxtemp$season)
+# maxtemp$season[maxtemp$season == "Winter2"] <- "Winter"
+# maxtemp$season <- factor(maxtemp$season)
 
-springtemp <- subset(maxtemp, month %in% c(3, 4, 5))
-summertemp <- subset(maxtemp, month %in% c(6, 7, 8))
-falltemp <- subset(maxtemp, month %in% c(9, 10, 11))
-wintertemp <- subset(maxtemp, month %in% c(12, 1, 2))
-
-
-
-
-springclean <- springtemp[-which(springtemp$weatherval < 5 & springtemp$forecastValue > 30), ]
-summerclean <- summertemp
-fallclean <- falltemp
-winterclean <- wintertemp[-which(wintertemp$weatherval > 95), ]
-winterclean <- winterclean[winterclean$absForecastDiff < 26, ]
-
-
+# springtemp <- subset(maxtemp, month %in% c(3, 4, 5))
+# summertemp <- subset(maxtemp, month %in% c(6, 7, 8))
+# falltemp <- subset(maxtemp, month %in% c(9, 10, 11))
+# wintertemp <- subset(maxtemp, month %in% c(12, 1, 2))
+# 
+# 
+# 
+# 
+# springclean <- springtemp[-which(springtemp$weatherval < 5 & springtemp$forecastValue > 30), ]
+# summerclean <- summertemp
+# fallclean <- falltemp
+# winterclean <- wintertemp[-which(wintertemp$weatherval > 95), ]
+# winterclean <- winterclean[winterclean$absForecastDiff < 26, ]
 
 
-maxtempall <- rbind(springclean, summerclean, fallclean, winterclean)
+
+
+#maxtempall <- rbind(springclean, summerclean, fallclean, winterclean)
 
 ## use only 1-day forecasts
-maxtemponeday <- subset(maxtempall, LengthForecastDayOnly == 1)
+maxtemponeday <- subset(maxtemp, LengthForecastDayOnly == 1)
 
 histWeather$Date <- as.Date(histWeather$Date, format = "%Y-%m-%d")
 
-maxtempwpreds <- merge(histWeather, maxtemponeday, 
-  by.x = c("Date", "AirPtCd"),
-  by.y = c("Date", "AirPtCd"))
+# maxtempwpreds <- merge(histWeather, maxtemponeday, 
+#   by.x = c("Date", "AirPtCd"),
+#   by.y = c("Date", "AirPtCd"))
 
 
-maxtemplags <- maxtempwpreds %>% dplyr::group_by(city) %>%
-  mutate(adjmeanhum = lag(Mean_Humidity),
-    adjmeanwind = lag(Mean_Wind_SpeedMPH),
-    adjmeandew = lag(MeanDew_PointF),
-    adjmeanpressure = lag(Mean_Sea_Level_PressureIn),
-    adjmeanvis = lag(Mean_VisibilityMiles),
-    adjmaxtemp = lag(Max_TemperatureF))
+# maxtemplags <- maxtempwpreds %>% dplyr::group_by(city) %>%
+#   mutate(adjmeanhum = lag(Mean_Humidity),
+#     adjmeanwind = lag(Mean_Wind_SpeedMPH),
+#     adjmeandew = lag(MeanDew_PointF),
+#     adjmeanpressure = lag(Mean_Sea_Level_PressureIn),
+#     adjmeanvis = lag(Mean_VisibilityMiles),
+#     adjmaxtemp = lag(Max_TemperatureF))
 
 
 
-maxtemplagscomp <- maxtemplags[complete.cases(maxtemplags$adjmeanhum, 
-  maxtemplags$adjmeanwind), ] 
+maxtemplagscomp <- maxtemp[complete.cases(maxtemp$adjmeanhum, 
+  maxtemp$adjmeanwind), ] 
+
+summary(maxtemplagscomp$Date)
 
 ## only use half of the data for model fitting. Save the other half for
 ## validation
 
-set.seed(070718)
-sampindeces <- sample(1:nrow(maxtemplagscomp), size = floor(nrow(maxtemplagscomp) / 2),
-  replace = FALSE)
+# set.seed(070718)
+# sampindeces <- sample(1:nrow(maxtemplagscomp), size = floor(nrow(maxtemplagscomp) / 2),
+#   replace = FALSE)
 
-maxtemplagstrain <- maxtemplagscomp[sampindeces, ]
-maxtemplagstest <- maxtemplagscomp[-sampindeces, ]
-modrand <- lmer(weatherval ~ forecastValue + adjmeanhum
-  + adjmeanwind + season + (season | city), 
-  data = maxtemplagstrain)
-ranef(modrand)
-summary(modrand)
+# maxtemplagstrain <- maxtemplagscomp[sampindeces, ]
+# maxtemplagstest <- maxtemplagscomp[-sampindeces, ]
+# modrand <- lmer(weatherval ~ forecastValue + adjmeanhum
+#   + adjmeanwind + season + (season | city), 
+#   data = maxtemplagstrain)
+# ranef(modrand)
+# summary(modrand)
 
+
+
+#### Train and test are now split into: train - 2014/09/01 - 2016/08/31, test - 2016/09/01 - 2017/09/01
+maxtemplagstrain <- subset(maxtemplagscomp, Date >= "2014-09-01" & Date <= "2016-08-31")
+nrow(maxtemplagstrain)
+summary(maxtemplagstrain$Date)
+maxtemplagstest <- subset(maxtemplagscomp, Date >= "2016-09-01" & Date <= "2017-09-01")
+summary(maxtemplagstest$Date)
 
 # From Erin: I have also included the previous day's max temp. If you plug in humidity and wind in order
 # to get a better prediction of tomorrow's max temp, I think it also makes sense to include 
